2ed54826d6cb62a134cd7643aeaa59d0048cfac2
Vaibhav Gaur <vaibhavgaur2352001@gmail.com>
2025-10-25 11:38:16 +0530

    Title update and CORS change

--- backend/src/main/java/com/cardgame/backend/controller/GameController.java
+++ backend/src/main/java/com/cardgame/backend/controller/GameController.java
@@ -14,7 +14,8 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/game")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+
 public class GameController {
 
     @Autowired
