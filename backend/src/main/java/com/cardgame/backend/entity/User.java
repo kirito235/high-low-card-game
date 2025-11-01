774405f79418b1b2fe5976dfc9798dba5458a3f0
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-11-01 12:54:59 +0530

    Feature: user entity,repo and request

---
+++ backend/src/main/java/com/cardgame/backend/entity/User.java
@@ -0,0 +1,23 @@
+package com.cardgame.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
