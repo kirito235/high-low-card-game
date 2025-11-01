c314a614f3f3975fbc4578e8c8cce6debb6c866c
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-11-01 18:23:51 +0530

    Feature: security filter check user from database

--- backend/src/main/java/com/cardgame/backend/config/GoogleProperties.java
+++ backend/src/main/java/com/cardgame/backend/config/GoogleProperties.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 public class GoogleProperties {
     private String clientId;
     private String clientSecret;
+    private String dummyPassword;
 }
--- backend/src/main/java/com/cardgame/backend/filter/JwtAuthFilter.java
+++ backend/src/main/java/com/cardgame/backend/filter/JwtAuthFilter.java
@@ -5,24 +5,23 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
-
-    public JwtAuthFilter(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -39,11 +38,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         if (jwt != null && jwtUtil.validateToken(jwt)) {
             String username = jwtUtil.extractUsername(jwt);
-
+            var userDetails = userDetailsService.loadUserByUsername(username);
             var authentication = new UsernamePasswordAuthenticationToken(
-                    new User(username, "", Collections.emptyList()),
-                    null,
-                    Collections.emptyList()
+                    new User(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities()),
+                    userDetails.getPassword(),
+                    userDetails.getAuthorities()
             );
 
             authentication.setDetails(
